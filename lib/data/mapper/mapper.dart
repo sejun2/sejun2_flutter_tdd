5c43d96f281d506afe700bf0810d2bdab92bd48a
sejun2 <qpfjf56@gmail.com>
2023-03-13 13:20:14 +0900

    update comment

--- lib/data/mapper/mapper.dart
+++ lib/data/mapper/mapper.dart
@@ -1,3 +1,5 @@
+/// Mapper is a class that maps an entity to a model and vice versa.
+/// T is a model type. E is an entity type.
 abstract class Mapper<T, E>{
   T asModel(E entity);
   E asEntity(T model);
